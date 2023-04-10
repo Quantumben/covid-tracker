d75ff448789f1f76241b70f60e2cefce793a110d
QuantumBen <omosabenedict@gmail.com>
2023-04-10 07:28:38 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
         extend: {},
         container: {
             center: true,
-            padding: '2rem',
+            padding: "2rem",
         },
     },
     plugins: [],
