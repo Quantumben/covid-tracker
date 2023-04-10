c9eead70bd3669a17a778c0508a021d090958bbb
QuantumBen <omosabenedict@gmail.com>
2023-04-10 07:47:03 +0100

    Fetch Covid info on console

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,22 @@
 <template>
- <div class="">
-    Hello World
- </div>
+  <div class="home">Hello World</div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView',
-  components:{}
-}
-
+  name: "HomeView",
+  components: {},
+  methods: {
+    async fetchCovidData(){
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created() {
+    //created is a lifecycle method in vuejs that runs before the app is been mounted
+    const data = await this.fetchCovidData()
+    console.log(data)
+  },
+};
 </script>
