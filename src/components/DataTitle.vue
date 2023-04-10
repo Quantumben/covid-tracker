8508e0510c4128c598fd0889ae96f5d55b2a63d9
QuantumBen <omosabenedict@gmail.com>
2023-04-10 08:37:49 +0100

    Format the date using a package named moment

--- src/components/DataTitle.vue
+++ src/components/DataTitle.vue
@@ -4,14 +4,21 @@
       {{ title }}
     </h2>
     <div class="text-2xl mt-4 mb-10">
-      {{ dataDate }}
+      {{ timestamp }}
     </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   name: "DataTitle",
   props: ["title", "dataDate"],
+  computed: {
+    timestamp: function() {
+        return moment(this.dataDate).format('MMMM Do YYYY, h:mm:ss a')
+    }
+  }
 };
 </script>
