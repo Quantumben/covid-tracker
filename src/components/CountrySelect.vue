3d03a4732ac95789e533456ed77d36a2ab26e32c
QuantumBen <omosabenedict@gmail.com>
2023-04-10 14:21:05 +0100

    Add country Component props

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -1,16 +1,22 @@
 <template>
-  <select v-model="selected" class="form-select mt-1 block w-full border p-3 rounded">
-  <option value="0">Select Country</option>
+  <select
+    v-model="selected"
+    class="form-select mt-10 block w-full border p-3 rounded"
+  >
+    <option value="0">Select Country</option>
+    <option v-for="country in countries" :value="country.ID" :key="country.ID">
+      {{ country.Country }}
+    </option>
   </select>
 </template>
 
 <script>
 export default {
-  name: "CountrySelect",
-  props: ["countries"],
+  name: 'CountrySelect',
+  props: ['countries'],
   data() {
     return {
-      selected: 0,
+      selected: 0
     };
   },
 };
